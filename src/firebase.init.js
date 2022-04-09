c05e9343d58882c79b66dd69b81d4e426d5a0694
Tamanna-akter <atamanna577@gmail.com>
2022-04-09 17:13:54 +0600

    working email-password verification

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB0uClj62LCPXGol3THKe_Ria_xftZoDFU",
+    authDomain: "email-password-authentic-62229.firebaseapp.com",
+    projectId: "email-password-authentic-62229",
+    storageBucket: "email-password-authentic-62229.appspot.com",
+    messagingSenderId: "183783257959",
+    appId: "1:183783257959:web:e430ed045b9178951ca1c2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
